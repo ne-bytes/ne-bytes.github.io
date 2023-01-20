e2f7875262004df978b30063773b21b2003b6643
Derek Graham <deejaygraham@gmail.com>
2023-01-20 17:01:35 +0000

    logos for sponsors

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,7 +2,7 @@ module.exports = {
     url: process.env.URL || 'http://localhost:8080', // nebytes.co.uk or nebytes.net
     siteName: 'NE Bytes',
     siteTitle: 'The .NET developer community in the North East of the UK',
-    siteDescription: 'NE Bytes is a user group covering the North East and Cumbria in the UK. We have technical meetings covering Development and IT Pro topics every month.',
+    siteDescription: 'Microsoft Technology Usergroup in the North East of England.',
     social: {
         tito: 'http://ti.to/ne-bytes/',
         twitter: 'https://twitter.com/NEBytes',
