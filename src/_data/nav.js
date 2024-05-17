d26548cb705929891ad79c1c1cc88cd6b133ce6f
Derek Graham <deejaygraham@gmail.com>
2024-05-17 14:45:01 +0100

    Create nav.js

---
+++ src/_data/nav.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    name: "Next Event",
+    navUrl: "/index.html#what",
+  },
+  {
+    name: "Location",
+    url: "/index.html#what",
+  },
+  {
+    name: "Code Of Conduct",
+    url: "/index.html#code",
+  },
+  {
+    name: "Sponsors",
+    url: "/index.html#sponsors",
+  },
+  {
+    name: "Events",
+    url: "/blog/",
+  }
+];
